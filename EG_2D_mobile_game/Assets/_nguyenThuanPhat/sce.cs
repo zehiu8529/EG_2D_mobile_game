3f012e88efcec3e57aec24ab90efdc9929f9b5cf
NTPhat-99 <72063596+NTPhat-99@users.noreply.github.com>
2021-06-07 16:43:08 +0700

    chinh lai giao dien

---
+++ EG_2D_mobile_game/Assets/_nguyenThuanPhat/sce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void IveBeenClicked(string scene)
+    {
+        //The scene you are loading should be included in the build settings
+        //Example, note without the .unity on the end of the scene name
+        SceneManager.LoadScene(scene);
+    }
+}
