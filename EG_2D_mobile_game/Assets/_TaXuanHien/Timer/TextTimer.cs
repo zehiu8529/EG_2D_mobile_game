af1583ff43346e228d6f279a33862ff54c874aef
hientx17 <hientx2019@gmail.com>
2021-06-25 17:57:51 +0700

    time
    cập nhập time

---
+++ EG_2D_mobile_game/Assets/_TaXuanHien/Timer/TextTimer.cs
@@ -0,0 +1,80 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextTimer : MonoBehaviour
+{
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private char characterSpliter = ':';
+
+    [Header("Logic")]
+    private float timer;
+    private bool isActive;
+
+    public GameObject Lose;
+
+    public void Update()
+    {
+        if (isActive)
+        {
+            timer += Time.deltaTime;
+            UpdateText();
+        }
+    }
+    private void UpdateText()
+    {
+
+        // Get the amount time since start
+        float seconds = (timer % 60);
+        float minutes = ((int)(timer / 60) % 60);
+        float hours = (int)(timer / 3600);
+        text.text = hours.ToString("00") + characterSpliter + minutes.ToString("00") + characterSpliter + seconds.ToString("00");
+        Debug.Log((int)timer);
+        Wingame();
+    }
+
+    public float TimeLose;
+
+    private void Wingame()
+    {
+        if ((int)timer == TimeLose)
+        {
+            Lose.SetActive(true);
+            isActive = !isActive;
+        }
+    }
+
+    public void StartTimer()
+    {
+        StartTimer(0);
+    }
+    public void StartTimer(float seconds)
+    {
+        isActive = true;
+        timer = seconds;
+        UpdateText();
+    }
+
+
+    //public void AddTime(float seconds)
+    //{
+    //    timer += seconds ;
+    //    
+    //    UpdateText();
+    //}
+
+    public void PauseTimer()
+    {
+        isActive = !isActive;
+    }
+
+    public void ResetTimer()
+    {
+        timer = 0;
+        UpdateText();
+    }
+    public float GetTimeSinceStart()
+    {
+        return timer;
+    }
+}
---
+++ EG_2D_mobile_game/Assets/_TaXuanHien/VictoryAndLose/VictoryAndLose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryAndLose : MonoBehaviour
+{
+    public void playgame(string scene)
+    {
+        //The scene you are loading should be included in the build settings
+        //Example, note without the .unity on the end of the scene name
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
