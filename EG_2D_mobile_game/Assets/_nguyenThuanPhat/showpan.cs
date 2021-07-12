00c7f080ad8c6662437b364dfd6d62785b24000f
NTPhat-99 <72063596+NTPhat-99@users.noreply.github.com>
2021-07-12 21:46:06 +0700

    giao dien

---
+++ EG_2D_mobile_game/Assets/_nguyenThuanPhat/showpan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showpan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel;
+    int counter;
+
+    public void showpanel()
+    {
+        counter++;
+        if(counter % 2==1)
+        {
+            Panel.gameObject.SetActive(false);
+        }else
+        {
+            Panel.gameObject.SetActive(true);
+        }
+        
+    }    
+}
